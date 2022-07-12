09da2bfcf0e26295b5861346adf72433eb2b06c3
brian-cairns <79775364+brian-cairns@users.noreply.github.com>
2022-07-12 12:37:43 -0400

    update

--- submitForm.js
+++ submitForm.js
@@ -4,3 +4,33 @@ const formName = 'IISSSession'
 console.log('form: ' + formName)
 let newForm = {}
 
+let clientName = document.querySelector('input#clientName')
+clientName.addEventListener('change', (e) => {
+	console.log('changed')
+	newForm.clientName = e.target.value;
+  console.log(newForm.clientName);
+})
+
+let date = document.querySelector('input#date')
+date.addEventListener('change', (e) => {
+	newForm.date = e.target.value;
+  console.log(newForm.date);
+})
+
+let start = document.querySelector('input#start')
+start.addEventListener('change', (e) => {
+	newForm.start = e.target.value;
+  console.log(newForm.start);
+})
+
+let end = document.querySelector('input#end')
+end.addEventListener('change', (e) => {
+	newForm.end = e.target.value;
+  console.log(newForm.end);
+})
+
+let technicianName = document.querySelector('input#technicianName')
+technicianName.addEventListener('change', (e) => {
+	newForm.technicianName = e.target.value;
+  console.log(newForm.technicianName);
+})
